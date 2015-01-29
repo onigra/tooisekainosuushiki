1e252ffe145e83963ed4adfbdcdb157fbb582189
onigra <3280467rec@gmail.com>
2015-01-29 17:23:57 +0900

    fixxxxxxxxxxxxxxxxxxxxxx

--- lib/suushiki.rb
+++ lib/suushiki.rb
@@ -1,4 +1,47 @@
 class Suushiki
   def self.exec(shiki)
+    ary = shiki.split(/(\*|\+|\&|\|)/)
+
+    while ary.include?("|") do
+      ary.each_with_index do |item, i|
+        if item == "|"
+          res = (ary[i - 1].to_i | ary[i + 1].to_i)
+          ary[i-1..i+1] = res
+          ary[ary.index res] = res.to_s
+        end
+      end
+    end
+
+    while ary.include?("&") do
+      ary.each_with_index do |item, i|
+        if item == "&"
+          res = (ary[i - 1].to_i & ary[i + 1].to_i)
+          ary[i-1..i+1] = res
+          ary[ary.index res] = res.to_s
+        end
+      end
+    end
+
+    while ary.include?("+") do
+      ary.each_with_index do |item, i|
+        if item == "+"
+          res = (ary[i - 1].to_i + ary[i + 1].to_i)
+          ary[i-1..i+1] = res
+          ary[ary.index res] = res.to_s
+        end
+      end
+    end
+
+    while ary.include?("*") do
+      ary.each_with_index do |item, i|
+        if item == "*"
+          res = (ary[i - 1].to_i * ary[i + 1].to_i)
+          ary[i-1..i+1] = res
+          ary[ary.index res] = res.to_s
+        end
+      end
+    end
+
+    ary[0]
   end
 end
